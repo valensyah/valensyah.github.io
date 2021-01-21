9d74c6c2a99d2e7d19fa07ad3384a18c82127352
valensyah <67795340+valensyah@users.noreply.github.com>
2021-01-22 02:48:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$( document ).ready( function($) {
+    $( window ).scroll( function () {
+      if ( $(document).scrollTop() > 150 ) {
+        // Navigation Bar
+        $('.navbar').removeClass('animate__animated animate__fadeIn');
+        $('.navbar').addClass('animate__animated animate__fadeInDown bg-light fixed-top');
+      } else {
+        $('.navbar').removeClass('animate__animated animate__fadeInDown fixed-top bg-light');
+        $('.navbar').addClass('animate__animated animate__fadeIn');
+      }
+    });
+  
+  });
+
+  var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-spy'
+  })
